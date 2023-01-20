9ed7b9333d2416b91d895c6e205054c3904283f0
stevespreiz <65625597+stevespreiz@users.noreply.github.com>
2023-01-19 20:53:29 -0500

    Initialized repo with cookiecutter data science structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Machine learning approach to predicting aircraft engine failure in an open NASA dataset.',
+    author='Steven Spreizer',
+    license='',
+)
