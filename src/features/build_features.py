ac8de5ce2b2a18b487207e8f6573f40adfa48898
stevespreiz <65625597+stevespreiz@users.noreply.github.com>
2023-04-11 10:14:23 -0400

    Perform windowing on AE features

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -168,30 +168,33 @@ def validate(model, dataloader, epoch):
 
     return epoch_loss
 
-# train and validate the autoencoder neural network
-train_loss = []
-val_loss = []
-start = time.time()
-for epoch in range(EPOCHS):
-    print(f"Epoch {epoch+1} of {EPOCHS}")
-    train_epoch_loss = fit(model, train_dataloader, epoch)
-    val_epoch_loss = validate(model, test_dataloader, epoch)
-    train_loss.append(train_epoch_loss)
-    val_loss.append(val_epoch_loss)
-end = time.time()
-print(f"{(end-start)/60:.3} minutes")
-# save the trained model
-torch.save(model.state_dict(), f"./src/features/sparse_ae{EPOCHS}.pth")
+if(False):
+    # train and validate the autoencoder neural network
+    train_loss = []
+    val_loss = []
+    start = time.time()
+    for epoch in range(EPOCHS):
+        print(f"Epoch {epoch+1} of {EPOCHS}")
+        train_epoch_loss = fit(model, train_dataloader, epoch)
+        val_epoch_loss = validate(model, test_dataloader, epoch)
+        train_loss.append(train_epoch_loss)
+        val_loss.append(val_epoch_loss)
+    end = time.time()
+    print(f"{(end-start)/60:.3} minutes")
+    # save the trained model
+    torch.save(model.state_dict(), f"./src/features/sparse_ae{EPOCHS}.pth")
 
-# loss plots
-plt.figure(figsize=(10, 7))
-plt.plot(train_loss, color='orange', label='train loss')
-plt.plot(val_loss, color='red', label='validataion loss')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.legend()
-plt.savefig('./src/features/loss.png')
-plt.show()
+    # loss plots
+    plt.figure(figsize=(10, 7))
+    plt.plot(train_loss, color='orange', label='train loss')
+    plt.plot(val_loss, color='red', label='validataion loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.savefig('./src/features/loss.png')
+    plt.show()
+
+torch.load("./src/features/sparse_ae5.pth")
 
 features_train = np.zeros((train_data.shape[0],16))
 features_test = np.zeros((test_data.shape[0],16))
@@ -217,4 +220,54 @@ print("Features processed, now saving to file")
 
 np.savetxt("./data/processed/non-linear_features_train1.txt",features_train,delimiter=",")
 np.savetxt("./data/processed/non-linear_features_test1.txt",features_test,delimiter=",")
+
+num_units = int(np.max(features_train[:,0]))
+
+#create windowed train data sets as in paper (lots of storage for this one)
+N_tws = np.array([0,50,20,30,15])
+# N_tw = N_tws[i]
+N_tw = 50
+
+train_starts = np.zeros((num_units,1))
+train_lengths = np.zeros((num_units,1))
+#mark start locations in numpy mat for each unit and number of entries
+ind = 0
+for n in range(1,num_units+1):
+    while(features_train[ind,0] != n):
+        ind += 1
+        # train_lengths[n-1] += 1
+    train_starts[n-1] = ind
+    # train_lengths[n-1] += 1
+for n in range(1,num_units):
+    train_lengths[n-1] = train_starts[n] - train_starts[n-1]
+train_lengths[num_units - 1] = features_train.shape[0] - train_starts[num_units-1]
+ind = 0
+# for n in range(num_units_test):
+#     while(test_file[ind,0] != n):
+#         ind += 1
+#         train_lengths[n] += 1
+#     test_starts[n] = ind
+#     test_lengths[n] += 1
+
+# print(train_lengths)
+train_num_windows = train_lengths - N_tw + 1
+# print(train_num_windows)
+
+#if want to save the windowed data (big file)
+if(True):
+    train_windowed = np.zeros((int(np.sum(train_num_windows))*N_tw,features_train.shape[1]))
+    row = 0
+    for n in range(num_units):
+        for j in range(int(train_num_windows[n])):
+            # print(f"n: {n}")
+            # print(f"j: {j}")
+
+            start = int(train_starts[n]) + j
+            end = start + N_tw
+
+        
+            train_windowed[row:(row+N_tw),:] = features_train[start:end,:]
+            row += N_tw
+    #this is very big file but hopefully can copy to GPU once and then whole thing runs fast
+    np.savetxt("./data/processed/windowed_aefeaturestrain1.txt",train_windowed,delimiter=",")
 
